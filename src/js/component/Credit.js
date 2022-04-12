3216766ccd876630269eba4ddbc6fec7a2b5384f
Isaiah Desta <areandd@protonmail.com>
2022-04-12 15:15:13 +0000

    minor alteration, for the algo.

--- src/js/component/Credit.js
+++ src/js/component/Credit.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Credit = (props) => {
-	return (
-		<div>
-			
-		</div>
-	);
+	return <div></div>;
 };
 
 export default Credit;
--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Navbar = (props) => {
-	return (
-		<div>
-			
-		</div>
-	);
+	return <div></div>;
 };
 
 export default Navbar;
